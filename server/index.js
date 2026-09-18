df000a6f4f80479fcb66493159e98f37c5929ec6
Lokesh Sharan <kumarlokeshsharan@gmail.com>
2026-09-18 17:28:38 +0530

    Fix Express 5 wildcard routing error

--- server/index.js
+++ server/index.js
@@ -248,7 +248,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 // Fallback for React Router (Single Page App)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
